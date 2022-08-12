c7f976a4d18faba830ab4cd15357829840eea2de
ogisetiawan <ogisetiawan21@gmail.com>
2022-08-12 10:58:55 +0700

    feat: add module react ui component

---
+++ module/Modules - Noted.php
@@ -0,0 +1,20 @@
+<?php
+//@ PENGENALAN REACT
+//? react adalah library javascript/front end framework, digunakan karena berukuran kecil
+//? Komponen membuat antarmuka yang dibangun menggunakan React sangat reusable.
+//? Pembuatan antarmuka pengguna dapat lebih cepat. Anda bisa menuliskan “layaknya” sintaksis HTML di dalam kode JavaScript
+
+//@ KONSEP DASAR REACT
+//? react komponen; sama seperti elemen html, bedanya adalah hanya sebuah object javascript yg berisi elemen html
+//? react element; sebuah fungsi yang mengembalikan React element, reusable dapat digunakan kembali.
+//? composition; Proses menggabungkan banyak fungsi untuk menciptakan data yang lebih kompleks
+
+//@ REACT UI COMPONENT 
+//? react element; sama dgn DOM element, React.creactElement(/* type */, /* property */, /* content */);
+//? jsx; javascript expression, prosess react element -> html native
+//? react component; dengan membuat fungsi yang mengembalikan React element mengunakan html native
+//? component properties; tag custom html dan pemanggilan fungsi javascript yg dapat memberikan beberapa param props, children
+//? composite component; kumpulan beberapa element ui, dipecah menjadi bebrapa element
+//? npx create-react-app my-app; to create react-app
+//? npm start; to running the app
+//? npm run build; mem-build proyek ke dalam berkas HTML, CSS, dan JS secara statis
